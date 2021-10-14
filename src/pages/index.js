c80476c76dc23958cd1b3db082f312e640e5dcbb
Rhys Malyon <malyonr@gmail.com>
2021-10-14 19:33:15 +0900

    Update image

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,10 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>This is my Gatsby test website.</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="A man looking into the distance"
+        src="../images/Rhys Malyon.jpg"
       />
     </Layout>
   )
