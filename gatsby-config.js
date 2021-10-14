e959381b345df0cc7c30deb12e600c7d14d2c76c
Rhys Malyon <malyonr@gmail.com>
2021-10-14 20:11:06 +0900

    Create blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Rhys' Gatsby Site",
+    title: "My Awesome Test Website",
   },
   plugins: [
     "gatsby-plugin-image",
