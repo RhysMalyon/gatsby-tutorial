3388bee039ce0273af6857d74bd0f4550adcc716
Rhys Malyon <malyonr@gmail.com>
2021-10-15 13:37:57 +0900

    Create blog subfolder

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+import BlogPage from '.'
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="Super Cool Blog Posts">
+      <p>My blog post contents will go here (eventually).</p>
+    </Layout>
+  )
+}
+
+export default BlogPost
