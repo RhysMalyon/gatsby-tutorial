f5f35b576c387e58f37183c7f57b41f6882c8b92
Rhys Malyon <malyonr@gmail.com>
2021-10-15 15:13:07 +0900

    Import dynamic post data

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,13 +1,34 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../../components/layout'
-import BlogPage from '.'
 
-const BlogPost = () => {
+const BlogPost = ({ data }) => {
+  console.log(data)
+  const title = data.mdx.frontmatter.title
+  const date = data.mdx.frontmatter.date
+  const body = data.mdx.body
+
   return (
-    <Layout pageTitle="Super Cool Blog Posts">
-      <p>My blog post contents will go here (eventually).</p>
+    <Layout pageTitle={title}>
+      <small>{date}</small>
+      <MDXRenderer>
+        {body}
+      </MDXRenderer>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`
 
 export default BlogPost
